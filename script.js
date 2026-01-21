eb8b9e2de557b77d2c7e2997b08706009e0ec15c
AHAMED-JAMALDEEN-S <ahamedjamaldeens@gmail.com>
2026-01-21 08:41:12 +0530

    Added hover colors, active tabs, footer icons

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function loadPage(page, title) {
+function loadPage(page, title, activeLink) {
   fetch(page)
     .then(response => response.text())
     .then(data => {
       document.getElementsByClassName("content")[0].innerHTML = data;
+    
       document.title = title;
+
+      document.querySelectorAll('.nav-links a').
+      forEach(link => {
+        link.classList.remove('active');
+      });
+      activeLink.classList.add('active');
     })
     .catch(error => {
       document.getElementsByClassName("content")[0].innerHTML =
